2160f91c841ee49f415244af6953514528c679cc
GeneralCoder <jndx_taibai@163.com>
2014-04-04 14:18:04 +0800

    苟俊

--- src/com/example/testgitproject/New1Activity.java
+++ src/com/example/testgitproject/New1Activity.java
@@ -9,6 +9,8 @@ public class New1Activity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		super.onCreate(savedInstanceState);
+		super.onCreate(savedInstanceState);
 	}
 
 }
